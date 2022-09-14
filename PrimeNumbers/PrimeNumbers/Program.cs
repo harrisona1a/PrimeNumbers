07171763569b21138f19681a30aeeab7751270a4
harrisona1a <harrisona1aroverkids.org@gmail.com>
2022-09-13 22:59:58 -0400

    Completed Assignment

--- PrimeNumbers/PrimeNumbers/Program.cs
+++ PrimeNumbers/PrimeNumbers/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int prints = 0;
+            for (int i = 0; prints < 1000; i++)
+            {
+                int count = 0;
+                for (int j = 1; j < i+1; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        count++;
+                    }
+                }
+                if (count == 2)
+                {
+                    Console.WriteLine(i);
+                    prints++;
+                }
+            }
         }
     }
 }
